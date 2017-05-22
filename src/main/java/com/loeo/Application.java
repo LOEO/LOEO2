a5f9acd10e46f1d0a14fa56fc0d67497352ebeaf
LOEO <286269159@qq.com>
2017-05-22 18:40:14 +0800

    little fix

--- src/main/java/com/loeo/Application.java
+++ src/main/java/com/loeo/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LoeoApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LoeoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
