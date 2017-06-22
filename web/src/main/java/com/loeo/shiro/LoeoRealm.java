4a9f2662a34b5a4f9e0a7fcf10c27bf5c2ba8ebf
LOEO <286269159@qq.com>
2017-06-22 17:25:23 +0800

    little fix

--- web/src/main/java/com/loeo/shiro/LoeoRealm.java
+++ web/src/main/java/com/loeo/shiro/LoeoRealm.java
@@ -1,42 +1,64 @@
 package com.loeo.shiro;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.shiro.authc.AccountException;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authz.AuthorizationException;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.util.ByteSource;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * Created by LOEO on 2017/05/31 22:30
  */
 public class LoeoRealm extends AuthorizingRealm {
-    @Override
-    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
-        String username = (String) principalCollection.getPrimaryPrincipal();
-        //获取权限信息
-        SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
-        Set<String> roles = new HashSet<>();
-        simpleAuthorizationInfo.setRoles(roles);
-        return simpleAuthorizationInfo;
-    }
+	@Override
+	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+		if (principalCollection == null) {
+			throw new AuthorizationException("PrincipalCollection method argument cannot be null.");
+		}
+		String username = (String) getAvailablePrincipal(principalCollection);
+		//获取权限信息
+		SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
+		Set<String> roles = new HashSet<>();
+		Set<String> permissions = new HashSet<>();
+		simpleAuthorizationInfo.setRoles(roles);
+		simpleAuthorizationInfo.setStringPermissions(permissions);
+		return simpleAuthorizationInfo;
+	}
 
-    @Override
-    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
-        String username = (String) authenticationToken.getPrincipal();
-        //认证用户名密码
-        SimpleAuthenticationInfo simpleAuthenticationInfo = new SimpleAuthenticationInfo(
-                "lt",//用户名
-                "123",//密码
-                ByteSource.Util.bytes("lt"),//盐
-                getName()//realName
-        );
-        return simpleAuthenticationInfo;
-    }
+	@Override
+	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+		UsernamePasswordToken upToken = (UsernamePasswordToken) authenticationToken;
+		String username = upToken.getUsername();
+
+		// Null username is invalid
+		if (username == null) {
+			throw new AccountException("Null usernames are not allowed by this realm.");
+		}
+		String password = "111";//获取密码
+
+		if (password == null) {
+			throw new UnknownAccountException("No account found for user [" + username + "]");
+		}
+
+		//认证用户名密码
+		SimpleAuthenticationInfo simpleAuthenticationInfo = new SimpleAuthenticationInfo(
+				username,//用户名
+				password,//密码
+				ByteSource.Util.bytes("lt"),//盐
+				getName()//realName
+		);
+
+		return simpleAuthenticationInfo;
+	}
 }
