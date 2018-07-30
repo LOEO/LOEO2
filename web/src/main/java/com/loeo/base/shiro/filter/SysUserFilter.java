223b16ec747dc9a784ec9cc3dd3d8d1688039e80
Tony <286269159@qq.com>
2018-07-30 09:00:34 +0800

    fix

--- web/src/main/java/com/loeo/base/shiro/filter/SysUserFilter.java
+++ web/src/main/java/com/loeo/base/shiro/filter/SysUserFilter.java
@@ -2,12 +2,10 @@ package com.loeo.base.shiro.filter;
 
 import com.alibaba.fastjson.JSON;
 import com.loeo.base.Result;
-import com.loeo.utils.WebUtils;
 import org.apache.shiro.web.filter.authc.UserFilter;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
 
@@ -22,20 +20,11 @@ public class SysUserFilter extends UserFilter {
     protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
         httpServletResponse.setStatus(600);
-        if (WebUtils.isAjax((HttpServletRequest) request)) {
-            httpServletResponse.setCharacterEncoding("utf8");
-            httpServletResponse.setContentType("application/json; charset=UTF-8");
-            PrintWriter printWriter = response.getWriter();
-            printWriter.write(JSON.toJSONString(Result.failed("请先登录")));
-            printWriter.flush();
-        } else {
-            httpServletResponse.setCharacterEncoding("utf8");
-            httpServletResponse.setContentType("application/json; charset=UTF-8");
-            PrintWriter printWriter = response.getWriter();
-            printWriter.write(JSON.toJSONString(Result.failed("请先登录")));
-            printWriter.flush();
-            //httpServletResponse.sendRedirect(getLoginUrl());
-        }
+        httpServletResponse.setCharacterEncoding("utf8");
+        httpServletResponse.setContentType("application/json; charset=UTF-8");
+        PrintWriter printWriter = response.getWriter();
+        printWriter.write(JSON.toJSONString(Result.failed("请先登录")));
+        printWriter.flush();
         return false;
     }
 }
